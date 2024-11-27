5571da39a3a68942d0fc77c0aed2282358192ce8
spyrite <smirnov.p.y@gmail.com>
2024-11-27 22:05:39 +0600

    Выполнено ДЗ-6

---
+++ Module_2/module_2_6.py
@@ -0,0 +1,18 @@
+import random
+
+
+def check(i, j):
+    conditions = [a % (i + j) == 0, i != j, not f'{i}+{j}' in b, not f'{j}+{i}' in b]
+    return sum(conditions)
+
+
+a = random.choice(range(3, 20 + 1))
+b = []
+
+for num_1 in range(1, a):
+    for num_2 in range(1, a):
+        if check(num_1, num_2) == 4:
+            b.append(f'{num_1}+{num_2}')
+
+print(f'Число: {a}')
+print(f'Пароль: {''.join(b).replace('+', '')}')
