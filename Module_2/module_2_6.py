442db8436e4e0adb95297ee9b82cd6e20f2dc7b3
spyrite <smirnov.p.y@gmail.com>
2024-11-28 21:38:28 +0600

    Рассмотрена и пременена функция all()

--- Module_2/module_2_6.py
+++ Module_2/module_2_6.py
@@ -3,7 +3,7 @@ import random
 
 def check(i, j):
     conditions = [a % (i + j) == 0, i != j, not f'{i}+{j}' in b, not f'{j}+{i}' in b]
-    return sum(conditions)
+    return conditions
 
 
 a = random.choice(range(3, 20 + 1))
@@ -11,7 +11,7 @@ b = []
 
 for num_1 in range(1, a):
     for num_2 in range(1, a):
-        if check(num_1, num_2) == 4:
+        if all(check(num_1, num_2)):
             b.append(f'{num_1}+{num_2}')
 
 print(f'Число: {a}')
