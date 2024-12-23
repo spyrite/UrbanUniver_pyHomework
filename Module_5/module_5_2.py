13545379e3a28038975d08f5ec7305d4eab63a78
spyrite <smirnov.p.y@gmail.com>
2024-12-23 21:33:10 +0600

    Выполнено ДЗ-5-2 (урок "Специальные методы классов")

---
+++ Module_5/module_5_2.py
@@ -0,0 +1,28 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            for n_floor in range(1, new_floor+1):
+                print(n_floor)
+
+h1 = House('ЖК Кристалл', 20)
+h2 = House('Коттедж в Усть-Заостровке', 2)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
