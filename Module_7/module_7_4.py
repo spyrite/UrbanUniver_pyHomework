b47e3bc890949cbf176208d2aeec67190b066c50
spyrite <smirnov.p.y@gmail.com>
2025-01-12 11:20:10 +0600

    Выполнено ДЗ-7-4 по теме "Форматирование строк"

---
+++ Module_7/module_7_4.py
@@ -0,0 +1,31 @@
+def get_challenge_result():
+    if any([score_1 > score_2, score_1 == score_2 and team1_time < team2_time]):
+        return 'Победа команды Мастера кода!'
+    if any([score_1 < score_2, score_1 == score_2 and team1_time > team2_time]):
+        return 'Победа команды Волшебники данных!'
+    return 'Ничья'
+
+
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.2
+team2_time = 2153.31451
+tasks_total = score_1 + score_2
+time_avg = round((team1_time + team2_time) / (score_1 + score_2), 1)
+
+# Использование %
+print('В команде Мастера кода участников: %d!' % team1_num)
+print('Итого сегодня в командах участников: %d и %d!' % (team1_num, team2_num))
+print('---')
+
+# Использование format
+print('Команда Волшебники данных решила задач: {}!'.format(score_2))
+print('Волшебники данных решили задачи за {time} c!'.format(time=team2_time))
+print('---')
+
+# Использование f-строк
+print(f'Команды решили {score_1} и {score_2} задач')
+print(f'Результат битвы: {get_challenge_result()}')
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!')
